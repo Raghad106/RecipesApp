10d6d109443a70549ffc640a1c1b922d9b9bd77a
raghad106 <raghad.mahmoud.aboallkass@gmail.com>
2025-07-25 11:48:12 +0300

    Initial Commit

--- app/src/main/java/com/ucas/firebaseminiprojectcomplete/ui/HomeFragment.java
+++ app/src/main/java/com/ucas/firebaseminiprojectcomplete/ui/HomeFragment.java
@@ -44,6 +44,7 @@ public class HomeFragment extends Fragment implements OnItemListener.OnUserListe
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
+    private String name = "RAGHAD";
     private String mParam1;
     private String mParam2;
     private FragmentHomeBinding binding;
